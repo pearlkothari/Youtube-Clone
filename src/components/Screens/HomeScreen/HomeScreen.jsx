daadff05e4ac680a75cd716c6aaf3f004c4a85ef
Interns <interns@hybrisworld.com>
2022-06-07 20:59:05 +0530

    Video UI Added

--- src/components/Screens/HomeScreen/HomeScreen.jsx
+++ src/components/Screens/HomeScreen/HomeScreen.jsx
@@ -3,13 +3,14 @@ import { Container,Row,Col } from 'react-bootstrap';
 import CategoriesBar from '../../Categories-Bar/CategoriesBar';
 import Video from '../../Video/Video';
 
+
 function HomeScreen() {
   return (
     <Container>
         <CategoriesBar></CategoriesBar>
         <Row>
             {
-                [... new Array(20)].map(()=>{
+                [... new Array(40)].map(()=>{
                     return (
                         <Col lg={3} md={4}>
                             <Video></Video>
--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import {AiFillEye} from 'react-icons/ai';
+import './Video.scss'
 
 function Video() {
   return (
-    <div>Video</div>
+    <div className='video'>
+      <div className='thumbnail'>
+        <img src="https://i.ytimg.com/vi/G4JuopziR3Q/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAQX_KX_cqFiL-YyjuDc8iA6i3cnQ" alt=""></img>
+        <span>05:43</span>
+      </div>
+      <div className='channel'>
+          <img src='https://yt3.ggpht.com/fGvQjp1vAT1R4bAKTFLaSbdsfdYFDwAzVjeRVQeikH22bvHWsGULZdwIkpZXktcXZc5gFJuA3w=s68-c-k-c0x00ffffff-no-rj' alt=''></img>
+          <div className='title'>
+              <span className="Description">Exclusive Clip | Loki | Disney+</span>
+              <div className='details'>
+                  <span>Marvel Entertainment</span>
+                  <span>5M Views • 5 days ago</span>
+              </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
