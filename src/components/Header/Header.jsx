992cbb249f2863a25c80083897b8b46bee6d4eae
Interns <interns@hybrisworld.com>
2022-06-07 10:25:21 +0530

    Header-Commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import {FaBars} from "react-icons/fa"
+import {AiOutlineSearch} from "react-icons/ai"
+import {MdNotifications,MdApps} from "react-icons/md"
+import './Header.scss'
+import youtube_logo from '../../icons/youtube_logo.png';
+
 
 function Header() {
   return (
-    <div className='border border-dark'>Header</div>
+    <div className='header border border-dark'>
+      <FaBars className='Header-Bar'size={20}></FaBars>
+      <img 
+        src={youtube_logo} 
+        alt="YouTube" 
+        className="youtube_logo" 
+      />
+
+      <form>
+        <input type="text" placeholder='Search' />
+        <button type='submit'>
+            <AiOutlineSearch size={22}/>
+        </button>
+      </form>
+
+      <div className="_icons">
+        <MdNotifications size={22}/>
+        <MdApps size={22}/>
+        <img src="https://www.w3schools.com/w3images/avatar2.png" alt="avatar" className="avatar" />
+      </div>
+    </div>
   )
 }
 
